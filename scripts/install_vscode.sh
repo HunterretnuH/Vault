32d1757fcd7a9e788d8d87689ddf7715bbff07bf
HunterretnuH <m.gzela@gmail.com>
2026-05-17 12:15:14 +0200

    Fixes after testing deployment in Ubuntu VM

--- scripts/install_vscode.sh
+++ scripts/install_vscode.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash 
 
 echo "Installing other packages (VSCode)..."
+# TODO: Add check for $(arch) i.e. x86_64 for code_amd64.deb and add at least also ARM
 vscode_package_name="code_amd64.deb"
 wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O "$vscode_package_name"
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
 sudo apt install ./"$vscode_package_name"
 rm -f ./"$vscode_package_name"
 echo "Done."
