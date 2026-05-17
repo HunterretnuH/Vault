4d712dd9fc5850c174c79582af4c57cfabd2fdfc
HunterretnuH <m.gzela@gmail.com>
2026-05-18 01:01:26 +0200

    Some more updates.
    Added timeout to nix installation (default: Multi user).
    Made link_interception_tools.sh a common script connected to feature.

--- home/.hooks_helpers/vscode-extensions-export.sh
+++ home/.hooks_helpers/vscode-extensions-export.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
- if command -v mycommand >/dev/null 2>&1; then
+ if command -v code >/dev/null 2>&1; then
     code --list-extensions | sort > $XDG_CONFIG_HOME/Code/User/extensions.txt
 fi
 
