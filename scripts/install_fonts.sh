ebd09fc2903ad4cc2e0e58a5e554590f7a2d8f3d
HunterretnuH <m.gzela@gmail.com>
2026-05-17 17:16:15 +0200

    Some more fixes

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,9 +1,10 @@
 #/usr/bin/env bash
 
 echo "Installing fonts..."
-git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
+git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts
 cd nerd-fonts
 git sparse-checkout add patched-fonts/JetBrainsMono
+mkdir -p $HOME/.local/share/fonts/
 find . -name "JetBrainsMonoNerdFont*" | xargs -I {} cp {} ~/.local/share/fonts/
 cd ..
 rm -rf nerd-fonts
