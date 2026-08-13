16c635d7edbf53be4d92d56080f9cc7e0a09c2dd
Work <m.gzela@gmail.com>
2026-08-13 12:08:04 +0200

    Added work_terminal machine

---
+++ machines/work_terminal/scripts/init_git_config_for_vault_repo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd ~/.local/share/chezmoi
+git config user.name "michalg-work"
+git config user.e-mail "m.gzela@protonmail.com"
+git remote set-url origin git@github.com:HunterretnuH/Vault.git
